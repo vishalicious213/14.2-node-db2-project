62856d2504a9fc4226600132930ece9ee20f7e95
Vish Singh <vishalicious213@gmail.com>
2020-08-22 23:08:22 -0700

    drafted request handler for getting all cars and getting car by ID. neither returns data, but will return text responses during testing

--- carRouter.js
+++ carRouter.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const db = require('./data/config')
 
-router.get('/', (req, res) => {
-    res.status(200).send('GET cars data endpoint')
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await db.select("*").from("cars")
+        res.status(200).json(cars)
+        // res.status(200).send('GET cars data endpoint')
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await db.select("*").from("cars").where("id", req.params.id).limit(1)
+        res.status(200).json(car)
+        // res.status(200).send('GET car by ID')
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const carRouter = require('./carRouter');
 const server = express();
 
+server.use(express.json());
 server.use('/api', carRouter);
 
 server.get('/', (req, res) => {

9d998dcd362457ae534925354b556c035800ad32
Vish Singh <vishalicious213@gmail.com>
2020-08-22 17:53:51 -0700

    added up & down functions to create_cars migration

--- data/migrations/20200822173740_create-cars.js
+++ data/migrations/20200822173740_create-cars.js
@@ -1,8 +1,17 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cars', tbl => {
+        // create primary key called id
+        tbl.increments();
+        // VIN, make, model, and mileage
+        tbl.text('vin').unique().notNullable();
+        tbl.text('make').notNullable();
+        tbl.text('model').notNullable();
+        tbl.decimal('mileage').notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+    // drop the whole table
+    return knex.scheme.dropTableIfExists('cars');
 };

18b762ce197a32f5086797f057ec5365e2964e0f
Vish Singh <vishalicious213@gmail.com>
2020-08-23 14:30:34 -0700

    POST new car works. MVP.

--- carRouter.js
+++ carRouter.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('./data/config')
 
+// GET all cars
 router.get('/', async (req, res, next) => {
     try {
         const cars = await db.select("*").from("cars")
@@ -12,11 +13,34 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+// GET car by ID
 router.get('/:id', async (req, res, next) => {
     try {
         const car = await db.select("*").from("cars").where("id", req.params.id).limit(1)
         res.status(200).json(car)
         // res.status(200).send('GET car by ID')
+    } catch (error) {
+        next(error)
+    }
+})
+
+// POST new car
+router.post('/', async (req, res, next) => {
+    try {
+        const newCarID = await db
+
+        .insert({
+            vin: req.body.vin,
+            make: req.body.make,
+            model: req.body.model,
+            mileage: req.body.mileage,
+            transmission: req.body.transmission,
+            titleStatus: req.body.titleStatus
+        })
+        .into("cars")
+
+        const viewCar = await db("cars").where("id", newCarID).limit(1)
+        res.status(201).json(viewCar)
     } catch (error) {
         next(error)
     }
